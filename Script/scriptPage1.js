1bc3254fa3d895b7c8667623c33da631825e5eeb
bino1432 <bernardozplz@gmail.com>
2024-03-27 16:51:37 -0300

    SIte

--- Script/scriptPage1.js
+++ Script/scriptPage1.js
@@ -8,13 +8,33 @@ changeBtn.addEventListener("click", () => {
 btn.addEventListener("click", fetchApi());
 
 function updateInterface(data) {
-    const lampada = document.getElementById("lampada");
+
+    const led1 = document.getElementById("led1");
+    const led2 = document.getElementById("led2");
+    const led3 = document.getElementById("led3");
+
     if (data.led1 === "0") {
-        lampada.className = "off"
+        led1.className = "off"
     } else {
-        lampada.className = "on"
+        led1.className = "on"
     }
-    lampada.textContent = data.led1;
+
+    if (data.led2 === "0") {
+        led2.className = "off"
+    } else {
+        led2.className = "on"
+    }
+
+    if (data.led3 === "0") {
+        led3.className = "off"
+    } else {
+        led3.className = "on"
+    }
+
+    led1.textContent = data.led1;
+    led2.textContent = data.led2;
+    led3.textContent = data.led3;
+
     console.log(data)
 }
 
