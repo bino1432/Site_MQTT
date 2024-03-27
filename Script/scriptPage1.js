5102b61bd14b1c079b598ee1c83e88f575a4a1b1
Bernardo P Zapelini <94933673+bino1432@users.noreply.github.com>
2024-03-26 21:58:15 -0300

    Add files via upload

---
+++ Script/scriptPage1.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById("btn");
+const changeBtn = document.getElementById("changeBtn");
+
+changeBtn.addEventListener("click", () => {
+    post();
+});
+
+btn.addEventListener("click", fetchApi());
+
+function updateInterface(data) {
+    const lampada = document.getElementById("lampada");
+    if (data.led1 === "0") {
+        lampada.className = "off"
+    } else {
+        lampada.className = "on"
+    }
+    lampada.textContent = data.led1;
+}
+
+async function fetchApi() {
+    return await fetch('https://api-mqtt-seven.vercel.app/info')
+        .then(response => response.json())
+        .then(data => {
+            updateInterface(data)
+        })
+}
+
+function post() {
+    fetch('https://api-mqtt-seven.vercel.app/info', {
+        method: 'POST',
+        body: JSON.stringify({
+            led1: "1"
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+}
