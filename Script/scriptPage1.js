cbd42ac923504f46a1279b82f7714a79dc56a3eb
bino1432 <bernardozplz@gmail.com>
2024-03-27 18:40:03 -0300

    Site

--- Script/scriptPage1.js
+++ Script/scriptPage1.js
@@ -1,11 +1,4 @@
-const btn = document.getElementById("btn");
-const changeBtn = document.getElementById("changeBtn");
-
-changeBtn.addEventListener("click", () => {
-    post();
-});
-
-btn.addEventListener("click", fetchApi());
+fetchApi();
 
 function updateInterface(data) {
 
@@ -46,24 +39,15 @@ async function fetchApi() {
         })
 }
 
-// function post() {
-//     fetch('https://api-mqtt-seven.vercel.app/info', {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             led1: "1"
-//         }),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//         .then(response => response.json())
-//         .then(data => console.log(data))
-//         .catch(error => console.log(error))
-// }
 function post() {
     fetch('https://api-mqtt-seven.vercel.app/info', {
         method: 'POST',
-        body: JSON.stringify({led1: '1'})  
+        body: JSON.stringify({
+            led1: "1"
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
         .then(response => response.json())
         .then(data => console.log(data))
